2194b67d0e9c44a5821c3875640368340c0f6b97
caobaoqi6040 <caobaoqi6040@gmail.com>
2025-08-11 18:08:42 +0800

    :sparkles: feat(frontend): 初始化 shadcn-vue

---
+++ frontend/vite.config.ts
@@ -0,0 +1,21 @@
+import {defineConfig} from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import path from "node:path";
+
+// https://vite.dev/config/
+export default defineConfig({
+    plugins: [
+        vue(),
+        tailwindcss(),
+        vueJsx(),
+        vueDevTools(),
+    ],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
+    },
+})
